93ced238fc878e77327e77fe4185a6fd055b39c7
FjellOverflow <fjelloverflow@protonmail.com>
2025-02-18 13:28:40 +0200

    fix upload script

--- upload.sh
+++ upload.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
 WEBUI_IP="10.11.99.1"
-
 FILE="$1"
-RED='\033[0;31m'
-GREEN='\033[0;32m' 
-NONE='\033[0m'
 
 exit_with_warning () {
-    echo -e "${RED}$1${NONE}"
+    echo -e "\033[0;31m$1\033[0m"
     exit 1
 }
 
+# check that filepath given and file exists
 if [ -z "$FILE" ]
 then
     exit_with_warning "No filepath supplied."
@@ -22,6 +19,7 @@ then
     exit_with_warning "File $FILE does not exist."
 fi
 
+# check that reMarkable web-ui is reachable
 ping -c 1 $WEBUI_IP > /dev/null 2>&1
 
 if [ $? -ne 0 ]
@@ -29,6 +27,8 @@ then
     exit_with_warning "Couldn't reach reMarkable. Is the device plugged in and 'USB connection' enabled in 'Settings' -> 'Storage'?"
 fi
 
+
+# upload via ReST
 curl -s -o /dev/null \
     "http://$WEBUI_IP/upload" \
     -H "Origin: http://$WEBUI_IP" \
@@ -37,9 +37,10 @@ curl -s -o /dev/null \
     -H "Connection: keep-alive" \
     -F "file=@$FILE;filename=$(basename "$FILE");type=application/pdf"
 
+
 if [ $? -eq 0 ]
 then
-    log_success "Upload successful"
+    echo -e "\033[0;32mUpload successful\033[0m"
 else
     exit_with_warning "Upload failed."
 fi
